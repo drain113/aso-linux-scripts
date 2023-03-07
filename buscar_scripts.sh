018279d1e80b1e1e803526a1805988c584e1f392
Guille <gmonmar2408@g.educaand.es>
2023-03-07 14:26:28 +0100

    Finalizado scripts

---
+++ buscar_scripts.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+clear
+
+# --------- Función ayuda ---------
+
+function ayuda {
+  echo "Este script sirve para encontrar los scripts del directorio home de un usuario."
+}
+
+while getopts "h" opt; do
+  case ${opt} in
+    h ) # Mostrar ayuda
+        ayuda
+        exit 0;;
+    \? ) # Exit 1 si la opción no es válida
+         echo "ERROR: Opción no válida: -$OPTARG" 1>&2
+         ayuda
+         exit 1;;
+  esac
+done
+
+# -----------------------------
+
+contar_sh() {
+  TOTAL=0
+
+  for dir_home in /home/*; do
+    if [ -d "$dir_home" ]; then
+      USER=$(basename "$dir_home")
+      # Busca los archivos con extensión .sh en el directorio home.
+      CONTAR=$(find "$dir_home" -name "*.sh" | wc -l)
+      if [ "$CONTAR" -gt 0 ]; then
+        echo "Hay $TOTAL scripts en el directorio home"
+      fi
+    fi
+  done
+}
+
+contar_sh_user() {
+  read -p "Ingrese el nombre del usuario: " USER
+  dir_user="/home/$USER"
+  if [ -d "$dir_user" ]; then
+    CONTAR=$(find "$dir_user" -name "*.sh" | wc -l)
+    if [ "$CONTAR" -gt 0 ]; then
+      echo "$USER: $CONTAR scripts de Bash."
+    else
+      echo "No se encontraron scripts de Bash para el usuario $USER."
+    fi
+  else
+    echo "El usuario $USER no tiene una carpeta home."
+  fi
+}
+
+# -------- Menú -------- 
+
+while true; do
+  echo "Seleccione una opción:"
+  echo "1. Total de scripts del home global."
+  echo "2. Total de scripts de un usuario."
+  echo "3. Salir."
+  read -p "Selecciona una opción (1-3): " OPTION
+  clear
+  case $OPTION in
+    1) contar_sh;;
+    2) contar_sh_user;;
+    3) echo "Cerrando..."
+      exit;;
+  esac
+done
+
+# ----------------------
---
+++ numero_primo.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+clear
+
+# --------- Función ayuda ---------
+
+function ayuda {
+  echo "Este script sirve para determinar los números primos entre dos."
+}
+
+while getopts "h" opt; do
+  case ${opt} in
+    h ) # Mostrar ayuda
+        ayuda
+        exit 0;;
+    \? ) # Exit 1 si la opción no es válida
+         echo "ERROR: Opción no válida: -$OPTARG" 1>&2
+         ayuda
+         exit 1;;
+  esac
+done
+
+# -----------------------------
+
+if [ $# -ne 2 ]; then
+  echo "ERROR: Por favor, introduce dos argumentos (números)."
+  ayuda
+  exit 1
+fi
+
+# Determinar argumentos como variables
+num1=$1
+num2=$2
+
+# Checkear argumentos
+
+# Que sea número entero
+if ! [[ $num1 =~ ^[0-9]+$ ]] || ! [[ $num2 =~ ^[0-9]+$ ]] || [ $num1 -lt 0 ] || [ $num2 -lt 0 ]; then
+  echo "Por favor, introduce un número entero."
+  ayuda
+  exit 1
+fi
+
+# Que el primero numero sea menor / igual al segundo
+if [ $num1 -gt $num2 ]; then
+  echo "Error: El primer número debe ser menor o igual que el segundo número."
+  ayuda
+  exit 1
+fi
+
+# Checkear si es primo
+
+function primo {
+  if [ $1 -lt 2 ]; then
+    return 1
+  fi
+  for (( i=2; i<$1; i++ )); do
+    if [ $(($1%i)) -eq 0 ]; then
+      return 1
+    fi
+  done
+  return 0
+}
+
+# Recorremos los números entre num1 y num2 y verificamos si son primos
+for (( i=$num1; i<=$num2; i++ )); do
+  if primo $i; then
+    echo $i
+  fi
+done
